96c6ee87afafac2a6035d74d97db86b7c22a518b
changliu0828 <changliu0828@gmail.com>
2017-06-03 16:32:23 +0800

    nagamax version

--- position.py
+++ position.py
@@ -1,32 +1,102 @@
-
 class Position:
-    
     def __init__(self):
         self.board = []
         self.macroboard = []
-    
+
     def parse_field(self, fstr):
         flist = fstr.replace(';', ',').split(',')
-        self.board = [ int(f) for f in flist ]
-    
+        self.board = [int(f) for f in flist]
+
     def parse_macroboard(self, mbstr):
         mblist = mbstr.replace(';', ',').split(',')
-        self.macroboard = [ int(f) for f in mblist ]
-    
+        self.macroboard = [int(f) for f in mblist]
+
     def is_legal(self, x, y):
-        mbx, mby = x/3, y/3
-        return self.macroboard[3*mby+mbx] == -1 and self.board[9*y+x] == 0
+        mbx, mby = x / 3, y / 3
+        return self.macroboard[3 * mby + mbx] == -1 and self.board[9 * y + x] == 0
 
     def legal_moves(self):
-        return [ (x, y) for x in range(9) for y in range(9) if self.is_legal(x, y) ]
-        
-    def make_move(self, x, y, pid):
-        mbx, mby = x/3, y/3
-        self.macroboard[3*mby+mbx] = -1
-        self.board[9*y+x] = pid
-        
+        return [(x, y) for x in range(9) for y in range(9) if self.is_legal(x, y)]
+
+    def make_move(self, move, pid, myid, oppid):
+        (x, y) = move
+        self.board[9 * y + x] = pid
+        mbx, mby = x / 3, y / 3
+        if self.is_occupied(mbx, mby, myid, oppid):
+            self.macroboard[3 * mby + mbx] = pid
+        else:
+            self.macroboard[3 * mby + mbx] = 0
+
+        next_mbx, next_mby = x % 3, y % 3
+        if self.macroboard[3 * next_mby + next_mbx] == 0 or self.macroboard[3 * next_mby + next_mbx] == -1:
+            self.macroboard[3 * next_mby + next_mbx] = -1
+        else:
+            self.macroboard = [(-1 if x==0 else x) for x in self.macroboard]
+        #print('make_move')
+        #print(x, y, self.macroboard)
+
     def get_board(self):
         return ''.join(self.board, ',')
 
     def get_macroboard(self):
         return ''.join(self.macroboard, ',')
+
+    def evaluate(self, myid, oppid):
+        local_boards = [[self.board[9 * iy + ix] for iy in range(y * 3, y * 3 + 3) for ix in range(x * 3, x * 3 + 3)]
+                        for x in range(3) for y in range(3)]
+        return self.board_score(self.macroboard, myid, oppid) * 100 + sum([self.board_score(b, myid, oppid) for b in local_boards])
+
+    def board_score(self, b, myid, oppid):
+        lines = [[(0, 0), (1, 0), (2, 0)], #r0
+                 [(0, 1), (1, 1), (2, 1)], #r1
+                 [(0, 2), (1, 2), (2, 2)], #r2
+                 [(0, 0), (0, 1), (0, 2)], #c0
+                 [(1, 0), (1, 1), (1, 2)], #c1
+                 [(2, 0), (2, 1), (2, 2)], #c2
+                 [(0, 0), (1, 1), (2, 2)], #d0
+                 [(2, 0), (1, 1), (0, 2)]] #d1
+        score = 0
+        line_scores = [self.line_score([b[y*3+x] for (x,y) in line], myid, oppid) for line in lines]
+        if max(line_scores) == 3:
+            score = 3
+        elif min(line_scores) == -3:
+            score = -3
+        else:
+            score = max(line_scores) + min(line_scores)
+        return score
+
+    def line_score(self, l, myid, oppid):
+        my_num = 0
+        opp_num = 0
+        for x in l:
+            if x == myid:
+                my_num = my_num + 1
+            elif x == oppid:
+                opp_num = opp_num + 1
+        if my_num != 0 and opp_num != 0:
+            return 0
+        return my_num - opp_num
+
+    def is_occupied(self, mbx, mby, myid, oppid):
+        lines = [[(0, 0), (1, 0), (2, 0)], #r0
+                 [(0, 1), (1, 1), (2, 1)], #r1
+                 [(0, 2), (1, 2), (2, 2)], #r2
+                 [(0, 0), (0, 1), (0, 2)], #c0
+                 [(1, 0), (1, 1), (1, 2)], #c1
+                 [(2, 0), (2, 1), (2, 2)], #c2
+                 [(0, 0), (1, 1), (2, 2)], #d0
+                 [(2, 0), (1, 1), (0, 2)]] #d1
+        b = [self.board[9 * iy + ix] for iy in range(mby * 3, mby * 3 + 3) for ix in range(mbx * 3, mbx * 3 + 3)]
+        line_scores = [self.line_score([b[y*3+x] for (x,y) in line], myid, oppid) for line in lines]
+        return max(line_scores) == 3 or min(line_scores) == -3
+    def is_game_over(self, myid, oppid):
+        lines = [[(0, 0), (1, 0), (2, 0)], #r0
+                 [(0, 1), (1, 1), (2, 1)], #r1
+                 [(0, 2), (1, 2), (2, 2)], #r2
+                 [(0, 0), (0, 1), (0, 2)], #c0
+                 [(1, 0), (1, 1), (1, 2)], #c1
+                 [(2, 0), (2, 1), (2, 2)], #c2
+                 [(0, 0), (1, 1), (2, 2)], #d0
+                 [(2, 0), (1, 1), (0, 2)]] #d1
+        line_scores = [self.line_score([self.macroboard[y * 3 + x] for (x, y) in line], myid, oppid) for line in lines]
+        return max(line_scores) == 3 or min(line_scores) == -3
--- randombot.py
+++ randombot.py
@@ -1,38 +1,32 @@
 from random import randint
+import time
+import copy
 
 class RandomBot:
     def __init__(self):
         self.myid = 1
         self.oppid = 2
     def get_move(self, pos, tleft):
-        lmoves = pos.legal_moves()
-        return max(lmoves, key=lambda x: self.static_evaluation(pos, x))
-    def static_evaluation(self, pos, move):
-        score = 0
-        (x, y) = move
-        #check center
-        if x % 3 ==1 and y % 3 == 1:
-            score = score + 1
-        #check local
-        local_board = [pos.board[i + j * 9] for i in range(x / 3 * 3, x / 3 * 3 + 3) for j in range(y / 3 * 3, y / 3 * 3 + 3)]
-        score = score + self.align_num(local_board, x % 3, y % 3) * 10
-        #check global
-        if score >= 20:
-            if (x / 3 == 1 and y / 3 == 1):
-                score = score + 100
-            score = score + self.align_num(pos.macroboard, x/3, y/3) * 1000
-        print(x, y, score)
-        return score
-    def align_num(self, local_board, local_x, local_y):
-        num_max = 0
-        delta = [[(1,0), (2,0)], [(0,1), (0,2)], [(1,1),(2,2)]]
-        for d in delta:
-            num = 0
-            for (dx, dy) in d:
-                if local_board[(local_x + dx) % 3 * 3 + (local_y + dy) % 3] == self.myid:
-                    num = num + 1
-                if num == 2:
-                    return num
-                if num > num_max:
-                    num_max = num
-        return num_max
+        tstart = int(round(time.time() * 1000))
+        best_score, best_move = self.negamax(pos, 2, 0, self.myid)
+        print(int(round(time.time() * 1000)) - tstart)
+        #print(best_score, best_move)
+        return best_move
+
+    def negamax(self, pos, max_depth, current_depth, pid):
+        if current_depth == max_depth or pos.is_game_over(self.myid, self.oppid):
+            return pos.evaluate(self.myid, self.oppid), None
+        best_move = None
+        best_score = float('-inf')
+        for move in pos.legal_moves():
+            new_pos = copy.deepcopy(pos)
+            new_pos.make_move(move, pid, self.myid, self.oppid)
+            recursed_score, current_move = self.negamax(new_pos, max_depth, current_depth + 1, self.change_pid(pid))
+            current_score = - recursed_score
+            if (current_score > best_score):
+                best_score = current_score
+                best_move = move
+        return best_score, best_move
+    def change_pid(self, pid):
+        return self.myid if pid == self.oppid else self.oppid
+
