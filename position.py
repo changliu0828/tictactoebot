6fd7ec09ece86fbbc1d6b77f988efeb354f8875c
changliu0828 <changliu0828@gmail.com>
2017-06-03 19:10:12 +0800

    version10 evaluation function update & bug fix

--- position.py
+++ position.py
@@ -20,6 +20,8 @@ class Position:
 
     def make_move(self, move, pid, myid, oppid):
         (x, y) = move
+        #if (x,y) == (8,7):
+            #print(self.get_macroboard())
         self.board[9 * y + x] = pid
         mbx, mby = x / 3, y / 3
         if self.is_occupied(mbx, mby, myid, oppid):
@@ -29,15 +31,19 @@ class Position:
 
         next_mbx, next_mby = x % 3, y % 3
         if self.macroboard[3 * next_mby + next_mbx] == 0 or self.macroboard[3 * next_mby + next_mbx] == -1:
+            self.macroboard = [(0 if i == -1 else i) for i in self.macroboard]
             self.macroboard[3 * next_mby + next_mbx] = -1
         else:
-            self.macroboard = [(-1 if x==0 else x) for x in self.macroboard]
+            self.macroboard = [(-1 if i==0 else i) for i in self.macroboard]
+        #if (x, y) == (8, 7):
+            #print(self.get_macroboard())
+            #print(self.get_board())
 
     def get_board(self):
         return ''.join(str(self.board))
 
     def get_macroboard(self):
-        return ''.join(self.macroboard, ',')
+        return ''.join(str(self.macroboard))
 
     def evaluate(self, myid, oppid):
         local_boards = [[self.board[9 * iy + ix] for iy in range(y * 3, y * 3 + 3) for ix in range(x * 3, x * 3 + 3)]
@@ -61,7 +67,9 @@ class Position:
         elif min(line_scores) == -9:
             score = -9
         else:
+            #print(max(line_scores), min(line_scores))
             score = max(line_scores) + min(line_scores)
+        #print(score)
         return score
 
     def line_score(self, l, myid, oppid):
@@ -91,7 +99,7 @@ class Position:
                  [(2, 0), (1, 1), (0, 2)]] #d1
         b = [self.board[9 * iy + ix] for iy in range(mby * 3, mby * 3 + 3) for ix in range(mbx * 3, mbx * 3 + 3)]
         line_scores = [self.line_score([b[y*3+x] for (x,y) in line], myid, oppid) for line in lines]
-        return max(line_scores) == 3 or min(line_scores) == -3
+        return max(line_scores) == 9 or min(line_scores) == -9
     def is_game_over(self, myid, oppid):
         lines = [[(0, 0), (1, 0), (2, 0)], #r0
                  [(0, 1), (1, 1), (2, 1)], #r1
